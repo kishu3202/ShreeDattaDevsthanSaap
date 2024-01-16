eba306544413a4dc8d7e60aeaafc3c1f5bc600d0
kishori pawar <you@example.com>
2024-01-16 18:59:51 +0530

    some changes

--- lib/drawer_screens/mantr_screen.dart
+++ lib/drawer_screens/mantr_screen.dart
@@ -1,135 +1,102 @@
-import 'dart:typed_data';
+// import 'package:flutter/material.dart';
+//
+//
+// class MantrScreen extends StatefulWidget {
+//   const MantrScreen({super.key});
+//
+//   @override
+//   State<MantrScreen> createState() => _MantrScreenState();
+// }
+//
+// class _MantrScreenState extends State<MantrScreen> {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('मंत्र'),centerTitle: true,),
+//     );
+//   }
+// }
 
-import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/cupertino.dart';
+
+
+// This is a minimal example demonstrating a play/pause button and a seek bar.
+// More advanced examples demonstrating other features can be found in the same
+// directory as this example in the GitHub repository.
+
+// This is a minimal example demonstrating a play/pause button and a seek bar.
+// More advanced examples demonstrating other features can be found in the same
+// directory as this example in the GitHub repository.
+
+import 'package:audio_session/audio_session.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:just_audio/just_audio.dart';
 
 class MantrScreen extends StatefulWidget {
-  const MantrScreen({super.key});
+  const MantrScreen({Key? key}) : super(key: key);
 
   @override
-  State<MantrScreen> createState() => _MantrScreenState();
+  MyAppState createState() => MyAppState();
 }
 
-class _MantrScreenState extends State<MantrScreen> {
-  int maxduration = 100;
-  int currentpos = 0;
-  String currentpostlabel = "00:00";
-  String audioasset = "assets/audio/omkrimkalikaynamh.mpeg";
-  bool isplaying = false;
-  bool audioplayed = false;
-  late Uint8List audiobytes;
+class MyAppState extends State<MantrScreen> with WidgetsBindingObserver {
+  final _player = AudioPlayer();
 
-  AudioPlayer player = AudioPlayer();
   @override
   void initState() {
-    Future.delayed(Duration.zero, () async {
-
-      ByteData bytes = await rootBundle.load(audioasset); //load audio from assets
-      audiobytes = bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
-      //convert ByteData to Uint8List
-
-      player.onDurationChanged.listen((Duration d) { //get the duration of audio
-        maxduration = d.inMilliseconds;
-        setState(() {
-
-        });
-      });
-      // player.onAudioPositionChanged.listen((Duration  p){
-      //   currentpos = p.inMilliseconds; //get the current position of playing audio
-      //
-      //   //generating the duration label
-      //   int shours = Duration(milliseconds:currentpos).inHours;
-      //   int sminutes = Duration(milliseconds:currentpos).inMinutes;
-      //   int sseconds = Duration(milliseconds:currentpos).inSeconds;
-      //
-      //   int rhours = shours;
-      //   int rminutes = sminutes - (shours * 60);
-      //   int rseconds = sseconds - (sminutes * 60 + shours * 60 * 60);
-      //
-      //   currentpostlabel = "$rhours:$rminutes:$rseconds";
-      //
-      //   setState(() {
-      //     //refresh the UI
-      //   });
-      // });
-
-    });
     super.initState();
+
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+    ));
+    //_init();
   }
+
+  Future<void> _init() async {
+    // Inform the operating system of our app's audio attributes etc.
+    // We pick a reasonable default for an app that plays speech.
+    final session = await AudioSession.instance;
+    await session.configure(const AudioSessionConfiguration.speech());
+    // Listen to errors during playback.
+    _player.playbackEventStream.listen((event) {},
+        onError: (Object e, StackTrace stackTrace) {
+          print('A stream error occurred: $e');
+        });
+    // Try to load audio from a source and catch any errors.
+    try {
+      // AAC example: https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.aac
+      // await _player.setAudioSource(AudioSource.uri(Uri.parse(
+      //       //     "https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3")));
+      _player.setAudioSource(AudioSource.asset("assets/audio/omkrimkalikaynamh.mpeg"));
+      _player.play();
+
+    } catch (e) {
+      print("Error loading audio source: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+
+    // Release decoders and buffers back to the operating system making them
+    // available for other apps to use.
+    _player.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('मंत्र'),centerTitle: true,),
-      body: Container(
-        margin: EdgeInsets.only(top:50),
-        child: Column(
-          children: [
-
-          Container(
-          child: Text(currentpostlabel, style: TextStyle(fontSize: 25),),
+    _init();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Text("hello"),
+        ),
       ),
-
-      Container(
-          child: Slider(onChanged: (value){},
-            value: double.parse(currentpos.toString()),
-            min: 0,
-            max: double.parse(maxduration.toString()),
-            divisions: maxduration,
-            label: currentpostlabel,
-            // onChanged: (double value) async {
-            //   int seekval = value.round();
-            //   int result = await player.seek(Duration(milliseconds: seekval));
-            //   if(result == 1){ //seek successful
-            //     currentpos = seekval;
-            //   }else{
-            //     print("Seek unsuccessful.");
-            //   }
-            // },
-          )
-      ),
-        Container(
-        child: Wrap(
-        spacing: 10,
-        children: [
-        ElevatedButton.icon(
-        onPressed: ()  {
-      if(!isplaying && !audioplayed) {
-        player.setSource();
-        player.play(source:AssetSource('assets/audio/omkrimkalikaynamh.mpeg'));
-      }
-    },
-    icon: Icon(isplaying?Icons.pause:Icons.play_arrow),
-    label:Text(isplaying?"Pause":"Play")
-    ),
-
-    ElevatedButton.icon(
-      onPressed: (){},
-    // onPressed: () async {
-    // int result = await player.stop();
-    // if(result == 1){ //stop success
-    // setState(() {
-    // isplaying = false;
-    // audioplayed = false;
-    // currentpos = 0;
-    // });
-    // }else{
-    // print("Error on stop audio.");
-    // }
-    // },
-    icon: Icon(Icons.stop),
-    label:Text("Stop")
-    ),
-    ],
-    ),
-    )
-
-    ],
-    )
-      )
-
     );
   }
 }
+
